999c3e52d16e7a42ad2f64f00b4c5cbdf1bad660
Luis <luiscoimbraeng@outlook.com>
2022-02-11 13:03:35 -0300

    css file addition

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import { useState, useMemo } from "react";
+import { useState, useMemo, useRef } from "react";
 import ColorComponent from "./components/ColorComponent";
-import { rgbToHex, hexToRgb, checkValidHex } from './utils/ColorLogic'
+import { rgbToHex, hexToRgb, checkValidHex } from "./utils/ColorLogic";
 
 export default function App() {
+  const inputEl = useRef(null);
   const [color, setColor] = useState("ff0000");
   const [subdivision, _] = useState(10);
   const primary_color = useMemo(() => hexToRgb(color), [color]);
 
   const validateAndSetColor = (color) => {
     if (checkValidHex(color)) {
-      setColor(color)
+      setColor(color);
     }
-  }
-
+  };
 
   const colors = [...Array(2 * subdivision + 1).keys()].map((i) => {
-    const division_number = i - subdivision;
-    //const end_color = HEXToVBColor("000000") //subdivision >= 0 ? HEXToVBColor("000000") : HEXToVBColor("ffffff")
+    const division_number = subdivision - i;
     const new_color = primary_color.map((p, i) => {
       const diff_from_reference = division_number > 0 ? 255 - p : p;
       return p + (division_number / subdivision) * diff_from_reference;
@@ -25,22 +24,18 @@ export default function App() {
   });
 
   return (
-    <div>
+    <>
       <header>
         <div>My personal color generator</div>
         <div>
-          <input value={color} onChange={(e) => validateAndSetColor(e.target.value)} />
-          <button>Send</button>
+          <input
+            ref={inputEl}
+            placeholder="ff0000"
+            onChange={(e) => validateAndSetColor(e.target.value)}
+          />
         </div>
       </header>
-      <div
-        style={{
-          display: "flex",
-          width: "100%",
-          flexWrap: "wrap",
-          alignItems: "stretch",
-        }}
-      >
+      <div className='palleteList'>
         {colors.map(([my_column, division_number]) => (
           <ColorComponent
             key={division_number}
@@ -49,6 +44,6 @@ export default function App() {
           />
         ))}
       </div>
-    </div>
+    </>
   );
 }
--- src/components/ColorComponent.js
+++ src/components/ColorComponent.js
@@ -1,38 +1,43 @@
-import { hexToRgb } from '../utils/ColorLogic'
+import { hexToRgb } from "../utils/ColorLogic";
 import { useState } from "react";
 
 export default function ColorComponent({ color, division_number }) {
   const sumRGB = hexToRgb(color).reduce((v, a) => v + a, 0);
   const fontColor = sumRGB > 383 ? "#000" : "#FFF";
-  const [successCopyMsg, setSuccessCopyMsg] = useState("");
+  const [showSuccessMsg, setshowSuccessMsg] = useState(false);
 
   const copyToClipBoard = (text) => {
     navigator.clipboard.writeText(text).then(
       function () {
-        setSuccessCopyMsg("Copiado!");
-        setTimeout(() => setSuccessCopyMsg(""), 1000);
+        setshowSuccessMsg(true);
+        setTimeout(() => setshowSuccessMsg(false), 1500);
       },
       function (err) {
         console.error("Async: Could not copy text: ", err);
       }
     );
   };
+
+  const coppiedClass = showSuccessMsg ? "copied active" : "copied"
+
   return (
     <div
+      className="singleColor"
       style={{
         color: fontColor,
         backgroundColor: color,
-        height: "120px",
-        minWidth: "150px",
-        paddingLeft: "30px",
-        cursor: "pointer",
-        flexGrow: 1,
       }}
       onClick={() => copyToClipBoard(color)}
     >
-      <p>{color}</p>
-      <p>{100 * division_number + "%"}</p>
-      <p>{successCopyMsg}</p>
+      <div >
+        <div>{100 * division_number + "%"}</div>
+        <div style={{ marginTop: "5px" }}>{color}</div>
+      </div>
+      
+        <div style={{ alignSelf: "center" }} className={coppiedClass} >
+          Copiado!
+        </div>
     </div>
+    /* {{ margin: '25px'}} */
   );
 }
